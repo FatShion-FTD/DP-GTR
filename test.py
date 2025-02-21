94b77c3949dfc316365d117f0f2dc50771cd3165
FatShion-FTD <mingchenli888@gmail.com>
2025-02-20 21:47:32 -0600

    Auto Update

--- test.py
+++
@@ -1,6 +0,0 @@
-from GTR import GTR
-from openai_generation import dp_paraphrase, generate
-gtr = GTR()
-input = "In which year, john f. kennedy was assassinated?"
-rewrites = gtr.gtr(input, dp_paraphrase, temperature=1.0)
-print(rewrites)
